85df97fdabeb5bf04e488d0378eaad20bc102257
IconicGIT <72080813+IconicGIT@users.noreply.github.com>
2022-10-06 10:32:04 +0200

    updated flock

--- AI exercises/AI/Seek exercise/Assets/Scrips/FlockAgent.cs
+++ AI exercises/AI/Seek exercise/Assets/Scrips/FlockAgent.cs
@@ -7,9 +7,23 @@ public class FlockAgent : MonoBehaviour
     public FlockController myManager;
     public Vector3 direction;
 
-    [SerializeField]
-    float speed;
+    public Vector3 cohesion = Vector3.zero;
+    public Vector3 align = Vector3.zero;
+    public Vector3 separation = Vector3.zero;
 
+    public float speed;
+
+    [SerializeField]
+    [Range(0.0f, 5.0f)]
+    float timerRef;
+
+    [SerializeField]
+    float timerRefVar;
+    float timer;
+
+    [SerializeField]
+    [Range(0, 180)]
+    float randomAdditionalAngle;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,49 +33,75 @@ public class FlockAgent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 cohesion = Vector3.zero;
-        Vector3 align = Vector3.zero;
-        Vector3 separation = Vector3.zero;
+        if (timer <= 0)
+        {
+                timer = Random.Range(timerRef - timerRefVar, timerRef + timerRefVar);
+                UpdateFlock();
+            
+        }
+
+        timer--;
+
+
+
+        Debug.DrawRay(transform.position, direction, Color.white);
+        Debug.DrawRay(transform.position, cohesion, Color.red);
+        Debug.DrawRay(transform.position, align, Color.green);
+        Debug.DrawRay(transform.position, separation, Color.blue);
+    }
+
+   public void UpdateFlock()
+    {
+        cohesion = Vector3.zero;
+        align = Vector3.zero;
+        separation = Vector3.zero;
+
+
         int num = 0;
+        Debug.Log("me: " + this);
         foreach (GameObject go in myManager.allAgents)
         {
             if (go != this.gameObject)
             {
-                float distance = Vector3.Distance(go.transform.position,
-                                                  transform.position);
+                float distance = Vector3.Distance(go.transform.position, transform.position);
+
                 if (distance <= myManager.neighbourDistance)
                 {
+                    
+
                     cohesion += go.transform.position;
 
                     align += go.GetComponent<FlockAgent>().direction;
 
-                    separation -= (transform.position - go.transform.position) / (distance * distance);
-
-
+                    separation += (transform.position - go.transform.position) / (distance * distance);
                     num++;
                 }
 
-
-
+                
             }
+           
         }
         if (num > 0)
         {
             align /= num;
 
+
             speed = Mathf.Clamp(align.magnitude, myManager.minSpeed, myManager.maxSpeed);
 
             cohesion = (cohesion / num - transform.position).normalized * speed;
+
+
         }
 
-        direction = (cohesion + align + separation).normalized * speed;
+        float randomAngle = Random.Range(-randomAdditionalAngle, randomAdditionalAngle);
+        Vector3 randomVec = new Vector3(Mathf.Cos(randomAngle * Mathf.Deg2Rad), 0, Mathf.Sin(randomAngle * Mathf.Deg2Rad));
+        direction = ((cohesion + align + separation).normalized + randomVec) * speed;
+        print("me: " + this + " cohesion:" + cohesion + " align: " + align + " separation: " + separation);
 
         transform.rotation = Quaternion.Slerp(transform.rotation,
                                           Quaternion.LookRotation(direction),
                                           myManager.rotationSpeed * Time.deltaTime);
-
         transform.Translate(0.0f, 0.0f, Time.deltaTime * speed);
-
 
     }
 
--- AI exercises/AI/Seek exercise/Assets/Scrips/FlockController.cs
+++ AI exercises/AI/Seek exercise/Assets/Scrips/FlockController.cs
@@ -12,57 +12,49 @@ public class FlockController : MonoBehaviour
 
     
     public GameObject[] allAgents;
-
     [SerializeField]
     Vector3 areaLimits;
-
-    
     [SerializeField]
     bool bounded;
-
     [SerializeField]
     bool randomize;
-
     [SerializeField]
     bool followLeader;
-
     [SerializeField]
     GameObject leader;
-
     [Header("Fish Settings")]
-
-    
     [Range(0, 20)]
     public float minSpeed;
-
-    
     [Range(0, 20)]
     public float maxSpeed;
-
-    
     [Range(0, 100)]
     public float neighbourDistance;
 
-    
     [Range(0, 20)]
     public float rotationSpeed;
 
     [SerializeField]
     float spawnRadius;
 
+
     // Start is called before the first frame update
     void Start()
     {
         allAgents = new GameObject[numAgents];
-        for (int i = 0; i < numAgents; ++i)
+        for (int i = 0; i < numAgents; i++)
         {
             Vector3 randomPos = new Vector3(Random.Range(-spawnRadius, spawnRadius), 0, Random.Range(-spawnRadius, spawnRadius));
             Vector3 pos = this.transform.position + randomPos;
             Vector3 randomize = new Vector3(Random.Range(0, 360), 0, Random.Range(0, 360));
+            
             allAgents[i] = (GameObject)Instantiate(flockAgent, pos,
                                 Quaternion.LookRotation(randomize));
+
+            allAgents[i].name = "agent " + i;
+            allAgents[i].GetComponent<FlockAgent>().speed = Random.Range(minSpeed, maxSpeed);
             allAgents[i].GetComponent<FlockAgent>().myManager = this;
             allAgents[i].transform.SetParent(this.transform);
+
         }
 
     }
